d9903b5a50ececd1bffba384e76a242a07de51ad
3dgiga <adrianogiovanelli@hotmail.com>
2026-04-15 12:26:36 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBHx_qPseO_P8UoAdo7lGSSFm7Zrbrb93c",
+  authDomain: "performance-auto-center.firebaseapp.com",
+  projectId: "performance-auto-center",
+  storageBucket: "performance-auto-center.firebasestorage.app",
+  messagingSenderId: "133229252732",
+  appId: "1:133229252732:web:b30a85225b2009b12cf9f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
